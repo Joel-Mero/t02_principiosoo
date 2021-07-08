e0c7bb5ef45db7668676bd17b3b57134505b6f18
Joel <Joel@DESKTOP-8B6OCT1>
2021-07-07 19:47:55 -0500

    Programa 1

---
+++ src/diagramasflujo/MainE1.java
@@ -0,0 +1,21 @@
+package diagramasflujo;
+
+import java.util.Scanner;
+
+public class MainE1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Ingrese valor del sueldo:");
+		Scanner teclado =new Scanner(System.in);//TECLADO
+		int S = teclado.nextInt();
+		
+		if(S<300) {
+			System.out.println("40");
+		}else {
+			System.out.println("0");		
+					}
+	}
+
+}
