e1871def1303401c93fa380f87e06ae52596f490
Joel <Joel@DESKTOP-8B6OCT1>
2021-07-09 19:48:55 -0500

    Ejer2

---
+++ src/diagramas/de/flujo/Main2.java
@@ -0,0 +1,16 @@
+package diagramas.de.flujo;
+
+import java.util.Scanner;
+
+public class Main2 {
+	//Imprimir en pantalla los 10 primero número impares 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int N=1 ; N<=19 ; N=N+2) {
+			System.out.println(N);
+		}
+	
+	}
+
+}
