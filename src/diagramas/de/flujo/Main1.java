b6e702e79903153f7f6c527a4f1708789659a85c
Joel <Joel@DESKTOP-8B6OCT1>
2021-07-09 19:46:49 -0500

    Ejer1

---
+++ src/diagramas/de/flujo/Main1.java
@@ -0,0 +1,20 @@
+package diagramas.de.flujo;
+
+import java.util.Scanner;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		//
+		// TODO Auto-generated method stub
+		
+		System.out.println("Ingrese un numero:");
+		Scanner teclado =new Scanner(System.in);//TECLADO	
+		int N = teclado.nextInt();	
+		
+		for(int C=N+1; C<=N+5; C++) {
+			System.out.println(C);
+		}
+		
+	}
+}
