d6a690bb0b8f01b65bb9194d198556913db62d89
Joel <Joel@DESKTOP-8B6OCT1>
2021-07-21 18:49:01 -0500

    Tipos de Datos

---
+++ src/tipos/de/datos/MainTiposdeDatos.java
@@ -0,0 +1,18 @@
+package tipos.de.datos;
+
+public class MainTiposdeDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short catindadEstudiantes= 1000; 
+		byte entradaCircuito = 0;
+		int canttidadCiudadanos = 100000;
+		long catidadMundo = 1000000000;
+		float sueldo = 1000.56f;
+		double saldoBanco = 100000000.67;
+		char opcionMenu = 'C';
+		boolean casado = true;
+		
+	}
+
+}
