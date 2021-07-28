25982c992ffa86af6f344f46b3c96eee74d22c31
Joel <Joel@DESKTOP-8B6OCT1>
2021-07-28 13:36:22 -0500

    Corrección ejercicio 1

---
+++ src/correccion/prueba/Ejer1.java
@@ -0,0 +1,34 @@
+package correccion.prueba;
+
+import java.util.Scanner;
+
+public class Ejer1 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int S=0;
+		int P=0;
+		int C=0;
+		
+		for (int i=1; i != 0; i++) {
+			System.out.println("Ingrese un número:");
+			Scanner teclado = new Scanner(System.in);
+			int N = teclado.nextInt();
+			
+			if (N==0) {
+				i = -1;
+			}else {
+				S = S + N;
+				C = C + 1;
+				P = S / C;
+			}
+		}
+		System.out.println("La suma es:" + S);
+		System.out.println();// SALTO DE LÍNEA
+		System.out.println("El promedio es:" + P);
+		System.out.println();// SALTO DE LÍNEA
+		System.out.println("Cantidad es:" + C);
+	}
+	
+}
