65d62bb6243ae3f16d8f4c7c03cd08ce49661f73
Joel <Joel@DESKTOP-8B6OCT1>
2021-07-30 17:28:49 -0500

    Ejercicio While

---
+++ src/correccion/prueba/EjerWhilw.java
@@ -0,0 +1,36 @@
+package correccion.prueba;
+
+import java.util.Scanner;
+
+public class EjerWhilw {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Ingrese un número:");
+		Scanner teclado = new Scanner(System.in);
+		int N = teclado.nextInt();
+		
+		int S = N;
+		float P = 0F;
+		int C = 0;
+	
+	while (N != 0) {
+		System.out.println("Ingrese un número:");
+		N = teclado.nextInt();
+		
+		S = S + N;
+		C = C + 1;
+		P = (S / C);
+		
+		}
+
+	System.out.println("La suma es:" + S);
+	System.out.println();
+	System.out.println("El promedio es:" + P);
+	System.out.println();
+	System.out.println("Cantidad es:" + C);
+	
+	}
+
+}
