5102a6d666aa59f76620b086e7b1e08000f4c55b
Joel <Joel@DESKTOP-8B6OCT1>
2021-07-30 17:52:53 -0500

    Ejercicio Do While

---
+++ src/correccion/prueba/EjerDoWhile.java
@@ -0,0 +1,37 @@
+package correccion.prueba;
+
+import java.util.Scanner;
+
+public class EjerDoWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Ingrese un número:");
+		Scanner entrada = new Scanner(System.in);
+		int N = entrada.nextInt();
+		
+		int SP = 0;
+		int SI = N;
+		int CI = 0;
+		float PI = 0F;
+		
+		do {
+			System.out.println("Ingrese un número:");
+			N = entrada.nextInt();
+			
+			if (N % 2 == 0) {
+				SP= (SP + N);
+			} else {
+				SI = SI + N;
+				CI = CI + 1;
+				PI = (SI / CI) + 1;
+			}
+		} while (N != -1);
+		
+		System.out.println("La suma es:" + SP);
+		System.out.println();// SALTO DE LÍNEA
+		System.out.println("El promedio es:" + PI);
+	}
+
+}
