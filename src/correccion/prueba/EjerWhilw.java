50b635a4706751eaf3d1f505feb1d82a0a17fcf6
Joel <Joel@DESKTOP-8B6OCT1>
2021-07-30 17:31:15 -0500

    Ejercicio While

--- src/correccion/prueba/EjerWhilw.java
+++ src/correccion/prueba/EjerWhilw.java
@@ -6,31 +6,31 @@ public class EjerWhilw {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		System.out.println("Ingrese un número:");
 		Scanner teclado = new Scanner(System.in);
 		int N = teclado.nextInt();
-		
+
 		int S = N;
 		float P = 0F;
 		int C = 0;
-	
-	while (N != 0) {
-		System.out.println("Ingrese un número:");
-		N = teclado.nextInt();
-		
-		S = S + N;
-		C = C + 1;
-		P = (S / C);
-		
+
+		while (N != 0) {
+			System.out.println("Ingrese un número:");
+			N = teclado.nextInt();
+
+			S = S + N;
+			C = C + 1;
+			P = (S / C);
+
 		}
 
-	System.out.println("La suma es:" + S);
-	System.out.println();
-	System.out.println("El promedio es:" + P);
-	System.out.println();
-	System.out.println("Cantidad es:" + C);
-	
+		System.out.println("La suma es:" + S);
+		System.out.println(); // Salto de línea
+		System.out.println("El promedio es:" + P);
+		System.out.println(); // Salto de línea
+		System.out.println("Cantidad es:" + C);
+
 	}
 
 }
