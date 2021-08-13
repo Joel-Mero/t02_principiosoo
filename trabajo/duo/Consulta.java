ad019a13d27478616025aac2c354dcb9d1ed7970
Joel-Mero <86030456+Joel-Mero@users.noreply.github.com>
2021-08-13 15:36:17 -0500

    Add files via upload

---
+++ trabajo/duo/Consulta.java
@@ -0,0 +1,15 @@
+package trabajo.duo;
+
+public class Consulta {
+
+	public void impri (String paciente, String medico, int consulta) {
+		System.out.println("----Consulta----");
+		System.out.print("Nombre paciente: ");
+		System.out.println(paciente);
+		System.out.print("Nombre medico: ");
+		System.out.println(medico);
+		System.out.print("Número de consulta: ");
+		System.out.println(consulta); //Prueba
+	}
+	
+}
